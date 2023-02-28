8545218102f6155a752b205fe86ef636d23abee1
Gaurav <79459872+gaurav-11018@users.noreply.github.com>
2023-02-28 21:42:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Community Rating of Moments !WORK UNDER PROGRESS! The Community Rating
+          of Moments is a platform where users can review Moments on a star
+          rating and add comments or additional information about the Moment.
+          The goal of the platform is to provide the community with a useful
+          guide for purchasing Moments by allowing users to share their
+          experiences and opinions.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
